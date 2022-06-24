346dc3a9f19cf2661c1a38804ac3d91acb14b8fe
Resul023 <73m6u7h@code.edu.az>
2022-06-24 20:25:09 +0400

    Task

---
+++ HeroBiz/HeroBiz/Models/Team.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeroBiz.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Position { get; set; }
+        public string Icon { get; set; }
+        public string IconUrl { get; set; }
+        public string Image { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
